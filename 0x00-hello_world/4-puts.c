c0f5a40e92f824d58746b5ac1371640a71facd6c
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-20 07:07:30 -0800

    fixed traling whitespaces on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,12 @@
 /**
  * main - Entry point
  *
+ * puts - outputs a string without null character
+ *
  * Return: Always 0 (success)
  */
- int main(void)
- {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
- }
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
