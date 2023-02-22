456c75f69dc696d6bc0641295725b286261d26cc
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-22 03:31:25 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+  * main - Entry point
+  *
+  * Description - Prints alphabets
+  *
+  * Return: Always 0 ( success)
+  */
+int main(void)
+{
+	int n = 97;
+	int b = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
