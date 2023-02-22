55aee2ea06298a82db370744da762353441ca965
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-22 04:11:03 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+  * main - Entry point
+  *
+  * Description - Prints numbers of base 10
+  *
+  * Return: Always 0 ( success)
+  */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+		return (0);
+}
