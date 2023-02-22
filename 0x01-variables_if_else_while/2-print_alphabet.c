ffa1d272af67284ad360ed4a527d89b5b75583a6
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-22 03:06:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+  * main - Entry point
+  *
+  * Description - Prints all lower case alphabets
+  *
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
