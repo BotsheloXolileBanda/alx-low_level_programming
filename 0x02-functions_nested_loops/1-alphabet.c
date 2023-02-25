36c2c8aa50f3ec7747e9b4277f2830a167c8b0ca
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-25 11:01:20 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet prints lowercase alphabets to stdout
+  *
+  * n - characters to print
+  *
+  * Return 0: Always (success)
+  */
+void print_alphabet(void)
+{
+	int n;
+	for ( n = 97 ; n <= 122 ; n++ )
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
