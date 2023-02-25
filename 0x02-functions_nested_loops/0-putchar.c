f3b1f90340ff78fd977dcf2d284e1fe7b6d44785
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-25 09:34:34 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,24 @@
-Empty
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Description - Code prints _putchar to standard output
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
