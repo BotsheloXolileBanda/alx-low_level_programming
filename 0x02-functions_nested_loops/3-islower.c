9f20eabf4e94f7d90a6c0f755a94e62732073ab8
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-26 05:44:35 -0800

    3-islower betty fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,14 @@
 /**
   * _islower - Checks for lowercase characters
   *
-  * c - character to be checked
+  * @c: character to be checked
   *
   * Return: 1 (if successful) 0 (if not successful)
   */
 int _islower(int c)
 {
-	if (c >= 'a' && c<= 'z')
-	return 1;
+	if (c >= 'a' && c <= 'z')
+	return (1);
 	else
-	return 0;
+	return (0);
 }
