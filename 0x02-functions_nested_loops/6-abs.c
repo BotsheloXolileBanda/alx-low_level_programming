97b6c3eebd0cf72f78c4dd10d2252115a162cda2
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-26 07:21:19 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+  * _abs - Prints absolute value of number
+  *
+  * @i: inputted number
+  *
+  * Return: (i) success
+  */
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else
+		return (i * -1);
+}
