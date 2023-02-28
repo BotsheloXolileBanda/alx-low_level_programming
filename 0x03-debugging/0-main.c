5c04a8f1132bc4191e116086a236905cf688bd94
BotsheloXolileBanda <bxbanda@gmail.com>
2023-02-28 08:14:56 -0800

    fixed 1-main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that print if integers is positive or negative sign
 * Return: 0
 */
 
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
