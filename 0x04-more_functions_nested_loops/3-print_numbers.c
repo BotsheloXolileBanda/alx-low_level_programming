26e8cb493abe470e44fa8e073348effafc8517d3
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-01 12:49:28 -0800

    3-print_number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+  * print_numbers - Prints numbers from zero to nine
+  *
+  * Return: Void
+  *
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
