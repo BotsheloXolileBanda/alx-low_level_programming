ce54834c078f19f83bc109c14890c6b65ffabb28
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-02 08:12:58 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+  * main - Entry point
+  *
+  * Description: Prints numbers from 0 to 100 except a few
+  *
+  * Return: Always (0) Success
+  *
+  */
+int main(void)
+{
+	int a, b;
+
+	b = 100;
+	for (a = 1; a <= b; a++)
+	{
+		if ((a % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((a % 3) == 0)
+			printf("Fizz ");
+		else if ((a % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
+
