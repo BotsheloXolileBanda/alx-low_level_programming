b5871fd3bfda85699fe43359215bcbc8128f331b
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-03 12:42:35 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+  * reset_to_98 - deferences a value to 98
+  *
+  * @n: pointer inputted
+  *
+  * Return: Void
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A project on pointers and manipulating strings as well as understanding arrays
