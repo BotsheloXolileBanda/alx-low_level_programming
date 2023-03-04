8b629d961fb16411a70e82cb04a7eaa50c573f03
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-04 11:31:52 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strncat - concatinates two strings
+  *
+  * @dest: Pointer to string
+  *
+  * @src: Pointer to string
+  *
+  * @n: pointer
+  *
+  * Return: Pointer to a string
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
