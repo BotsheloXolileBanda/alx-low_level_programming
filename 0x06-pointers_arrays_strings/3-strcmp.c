71ced9ec3b551b4d92d3a2831e2696e330fcf887
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-04 12:04:56 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+  * _strcmp - Compares strings
+  *
+  * @s1: first string
+  *
+  * @s2: second string
+  *
+  * Return: Comparison
+  */
+int _strcmp(char *s1, char *s2);
+{
+	int a;
+
+	a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
