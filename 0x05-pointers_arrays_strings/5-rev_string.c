1836f2832c09ac1825f814e8493721c8e781d4f8
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-04 06:32:45 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
   * rev_string - Reverses a string
   *
@@ -10,17 +9,17 @@
   */
 void rev_string(char *s)
 {
-	char rev = s[0];
+	char rev = S[0];
 	int counter = 0;
 	int i;
 
-	while (s[counter] != '\0')
+	while (S[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter, i++)
 	{
 		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		rev = S[i];
+		S[i] = S[counter];
+		S[counter] = rev;
 	}
 }
