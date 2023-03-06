ada7dc393e9423105cd33b861fc88de270bc76c3
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-06 10:39:51 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strchr - Checks for strings
+  *
+  * @s: input
+  *
+  * @c: input
+  *
+  * Return: Always 0 (success)
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] <= '\n'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
