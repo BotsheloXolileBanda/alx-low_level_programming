93fa5dd3d9163321d1a3a6bddffae877c9478ece
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-09 10:36:34 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - Prints a string to standard output
+  *
+  * @s: String to be printed
+  *
+  * Return: Void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+A project on the use of recursion in C to as an alternative to loops
