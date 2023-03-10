d26e5fc49e6cb1214717cf3ec43de26685ca111b
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-10 00:52:58 -0800

    1-print_rev_recursion and 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
+
 /**
   * _puts_recursion - Prints a string to standard output
   *
-  * @s: String to be printed
+  * @s: string to be printed
   *
   * Return: Void
   */
@@ -16,7 +17,6 @@ void _puts_recursion(char *s)
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
