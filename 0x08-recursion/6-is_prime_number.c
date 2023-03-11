29d072281b306920ee40733872dfbec989381e84
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-11 04:31:08 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+  * is_prime_number - Checks if a number is a prime number or not
+  *
+  * @n: Number inputted
+  *
+  * Return: 1 if prime and 0 otherwise
+  */
+int is_prime_number(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
