da464f0afedeae290ad3c5f681c7ed6ea7ae39ff
BotsheloXolileBanda <bxbanda@gmail.com>
2023-03-11 04:49:52 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * is_palindrome - Checks whether a string is a palindrome
+  *
+  * @s: String to be checked
+  *
+  * Return: 1 if palindrome and 0 otherwise
+  */
+int is_palindrome(char *s)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
