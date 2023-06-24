ef8d8aed88a3e5a1aaf17b5874a8eb38a87a43af
BotsheloXolileBanda <bxbanda@gmail.com>
2023-06-24 03:20:32 -0700

    Task 10 done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle shape.
+ * @size: The maginitude of the triangle.
+ * Return: No value.
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int k, p;
+
+		for (k = 1; k <= size; k++)
+		{
+			for (p = 1; p <= size; p++)
+			{
+				if (p < size - k)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
