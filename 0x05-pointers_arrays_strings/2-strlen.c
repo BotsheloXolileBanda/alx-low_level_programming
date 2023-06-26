860ca0331aa7bbd9d831566f16567219582ee999
BotsheloXolileBanda <bxbanda@gmail.com>
2023-06-26 06:31:27 -0700

    Task 2 completed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Determines the enth of a string.
+ * @s: The string to be checked.
+ *
+ * Return: The lenth of the string in integer form.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s+i) != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+	return (i);
+}
