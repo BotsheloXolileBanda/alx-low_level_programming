12c3aeeba189d995a2c1bcc5f693edd95884ab13
BotsheloXolileBanda <bxbanda@gmail.com>
2023-06-26 06:07:31 -0700

    Task 0 completed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Updates the value of inputted pointer to 98.
+  * @n: Inputted pointer.
+  *
+  * Return: No value.
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
