99099d795d7b81c25c89c492cd9c153cce9bcd27
BotsheloXolileBanda <bxbanda@gmail.com>
2023-07-02 00:01:53 -0700

    Task 3 ompleted

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,6 +7,20 @@
  *
  * Return: A number based on the results.
  */
-int _strcmp(char *s2, char *s1)
+int _strcmp(char *s1, char *s2)
 {
-	if (*
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else if (*s1 > *s2)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+	s1++;
+	s2++;
+}
