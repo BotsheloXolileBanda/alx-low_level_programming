5154ba9fb86f71069d67f9717dcda6b0d1201600
BotsheloXolileBanda <bxbanda@gmail.com>
2023-07-06 04:26:37 -0700

    Task 2 completed

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the lenth of the string using recursion.
+ * @s: The string to be checked.
+ *
+ * Return: No value.
+ */
+int _strlen_recursion(char *s)
+{
+	int u;
+
+	u = 0;
+
+	if (*(s + u) == '\0')
+	{
+		return (u);
+	}
+	else
+	{
+		while (*(s + u))
+		{
+			u++;
+		}
+		_strlen_recursion(s + 1);
+	}
+	return (u);
+}
