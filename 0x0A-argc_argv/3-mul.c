6939b5e0a73e36805789805f5718f96c62e37e37
BotsheloXolileBanda <bxbanda@gmail.com>
2023-07-07 23:41:13 -0700

    Task 3 completed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the product of two arguemnts passed to the program.
+ *
+ * @arc: Integer holding the count of arguments passed to the program.
+ * @argv: Array of strings passed as arguments to the program.
+ *
+ * Return: A value of 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if ((argc == 0) || (argc > 3))
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		product = (atoi)(argv[1]) * (atoi)(argv[2]);
+
+		printf("%d\n", product);
+	}
+	return (0);
+}
