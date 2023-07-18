6252f50e42b3ea46b07264e86d0ad19f24e6723b
BotsheloXolileBanda <bxbanda@gmail.com>
2023-07-17 20:58:11 -0700

    Task 5 completed

---
+++ 0x0E-strutures_typedef/5-free_dog.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdlib.h>
+
+void free_dog(dog_t *d)
+{
+	free(struct *dog_t);
+}
---
+++ 0x0E-strutures_typedef/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void free_dog(dog_t *d);
+#endif
