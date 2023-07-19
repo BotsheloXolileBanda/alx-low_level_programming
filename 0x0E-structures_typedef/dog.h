82887b1478e8bc4385bed40c8ae528bb97efee67
BotsheloXolileBanda <bxbanda@gmail.com>
2023-07-18 19:03:18 -0700

    Task 0 betty fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - A new type for dog infor.
+ * @name: Identifier in string.
+ * @age: How old dog is.
+ * @owner: Name of owner.
+ *
+ */
 struct dog
 {
 	char *name;
