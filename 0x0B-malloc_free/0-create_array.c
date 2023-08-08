92463d417d3a0fa1aa9d58412cc7127286b02af9
BotsheloXolileBanda <bxbanda@gmail.com>
2023-08-08 08:14:29 -0700

    Task 0 completed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Creates and also initialises an array of characters.
+ *
+ * @size: The length or byte count of the array.
+ * @c: The variable to fill the array.
+ * Return: A pointer to the new and fresh string.
+ */
+char *create_array(unsigned int size, char c);
+{
+	char *array_created;
+	int r;
+
+	array_created = malloc(sizeof(char) * size);
+
+	if (array_created == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		r = 0;
+
+		while (r < size)
+		{
+			array_created[r] = c;
+			r++;
+		}
+		return (array_created);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+A project on dynamic memory allocation mainly using malloc as well as free
