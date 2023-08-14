756f6c1041b54810d6810e6ea20e9a213485ea67
BotsheloXolileBanda <bxbanda@gmail.com>
2023-08-14 11:40:55 -0700

    Task 0 completed

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+A project on the workings of a new data type that is user defined: structures and use of typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - A struct called dog that I have defined.
+ *
+ * @name: The identifier for the animal.
+ * @age: A variable of float type for age.
+ * @owner: A string type variable for the variable owner.
+ * Description: A struct holding information about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
