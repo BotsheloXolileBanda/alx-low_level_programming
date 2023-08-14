071e6f033ab8135f9475abbb94f2f8e8d795c88b
BotsheloXolileBanda <bxbanda@gmail.com>
2023-08-14 12:42:03 -0700

    Task 1 redone logical error fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -14,6 +13,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	d->age = age;
+	d->owner = owner;
 }
