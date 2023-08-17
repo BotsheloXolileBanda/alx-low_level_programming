3526647d04e00ec1f02572de80fe0dda85305acc
BotsheloXolileBanda <bxbanda@gmail.com>
2023-08-16 20:59:54 -0700

    y2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
