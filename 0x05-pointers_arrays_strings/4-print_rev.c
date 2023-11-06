e99d0adbc1665972d5d0bd6099e2ff5a715a13e5
BotsheloXolileBanda <bxbanda@gmail.com>
2023-11-06 03:38:24 -0800

    Task 4 completed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Reverses a string with a newline character.
+ *
+ * @s: The string to reverse.
+ * Return: No value.
+ */
+void print_rev(char *s)
+{
+	int k;
+
+	k = 0;
+
+	while (*(s + k) != '\0')
+	{
+		k++;
+	}
+
+	while (k > 0)
+	{
+		_putchar(*(s + k));
+		k--;
+	}
+	_putchar('\n');
+}
