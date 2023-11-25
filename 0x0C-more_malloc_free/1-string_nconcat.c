255f3a19566f82fb8acf04ca09e4f3e2d71fbe18
BotsheloXolileBanda <bxbanda@gmail.com>
2023-11-24 22:54:41 -0800

    Task 1 done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Appends two strings to each other, subject to a limiter.
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The limiter.
+ * Return: Pointer to new string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int r, m, p, q;
+	char *newstr;
+
+	for (r = 0; *(s1 + r) != '\0'; r++)
+	{
+		;
+	}
+	for (p = 0; *(s2 + p) != '\0'; p++)
+	{
+		;
+	}
+	if (n < p)
+	{
+		newstr = malloc(sizeof(*newstr) * (r + n + 1));
+		if (!newstr)
+		{
+			return (NULL);
+		}
+		for (m = 0; m < r; m++)
+		{
+			*(newstr + m) = *(s1 + m);
+		}
+		for (q = 0; q < n; q++)
+		{
+			*(newstr + m++) = *(s2 + q);
+		}
+		*(newstr + m) = '\0';
+		return (newstr);
+	}
+	else
+	{
+		newstr = malloc(sizeof(*newstr) * (r + p + 1));
+		if (!newstr)
+		{
+			return (NULL);
+		}
+		for (m = 0; m < r; m++)
+		{
+			*(newstr + m) = *(s1 + m);
+		}
+		for (q = 0; q < p; q++)
+		{
+			*(newstr + m++) = *(s2 + q);
+		}
+		*(newstr + m) = '\0';
+		return (newstr);
+	}
+}
