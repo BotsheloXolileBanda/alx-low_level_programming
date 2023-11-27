58e2edc3b4d8804fcedcae52cdb2899f6fb1ebf6
BotsheloXolileBanda <bxbanda@gmail.com>
2023-11-27 02:48:08 -0800

    Remaining tasks done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Uses a predefined macro to print the name of the program it was compiled from.
+ *
+ * Return: A value of 0 or 1.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_LIKE
+#define FUNCTION_LIKE
+
+#define ABS(x) ((x < 0) ? (x * -1) : (x * 1))
+#endif
