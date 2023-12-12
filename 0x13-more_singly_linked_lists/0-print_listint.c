281e7f13aa9f8bdb97e417ab09bf7057229836ae
BotsheloXolileBanda <bxbanda@gmail.com>
2023-12-12 07:55:37 -0800

    Task 0 done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * print_listinit - Prints the contents of a linked list plus the number of nodes.
+  *
+  * @h: Pointer to the head node.
+  * Return: The number of nodes.
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t r;
+
+	for (r = 0; h != NULL; h = h->next, r++)
+	{
+		printf("%d\n", h->n);
+	}
+	return (r);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+A project further expounding on singly linked lists
