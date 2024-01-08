d77cfceeba1100c8c9c522e872057dd75bdab904
BotsheloXolileBanda <122918861+BotsheloXolileBanda@users.noreply.github.com>
2024-01-08 17:04:54 +0200

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/README.md
+++
@@ -1,1 +0,0 @@
-A project on the concept allowing for functions such as printf
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef VAR_H
-#define VAR_H
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, unsigned int n, ...);
-#endif
