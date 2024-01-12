4db598ace5738c337329207360cc0a12e152750e
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-12 03:15:10 -0800

    Task 1 completed first try

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - Prints the number of arguments it has.
+  *
+  * @argc: The argument count.
+  * @argv: THe argument strings.
+  * Return: 0 for success and otherwise failure.
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
