dc99db562563f65fc4224d56c16615d161500068
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-12 03:09:26 -0800

    Task 0 completed first attempt

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - Prints the executable name.
+  *
+  * @argc: The argument count variable.
+  * @argv: The argument strings.
+  * Return: 0 for success and otherwise failure.
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
