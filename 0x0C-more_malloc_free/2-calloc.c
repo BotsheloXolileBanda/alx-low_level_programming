6c35be0e2b4140f78386fb57e429f58910fff470
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-18 03:49:04 -0800

    Task 2 completed

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * _calloc - An implementation of calloc.
+  *
+  * @nmemb: Block count.
+  * @size: Size of each block.
+  * Return: Void pointer or NULL.
+  */
+void *_calloc(size_t nmemb, size_t size)
+{
+	size_t r;
+	char *calloced;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		calloced = malloc(sizeof *calloced * (nmemb * size));
+		if (!calloced)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (r = 0; r < (nmemb * size); r++)
+			{
+				calloced[r] = 0;
+			}
+			return (calloced);
+		}
+	}
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,5 @@
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(size_t nmemb, size_t size);
 #endif
