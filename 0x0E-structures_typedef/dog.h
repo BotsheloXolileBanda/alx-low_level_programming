85bff3cb3c9f95155100fd6f04b6ea9498b245e0
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-22 07:56:51 -0800

    Task 2 completed at first attempt

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,15 @@
   * @owner: The owner name.
   * Description: Name and owner are strings age is float.
   */
-struct dog
+/**
+  * dog_t - Typedef for struct dog.
+  */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
