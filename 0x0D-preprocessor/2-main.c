80eeb5cd2a754151ec6e1b6993b439f4311172bf
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-22 03:29:49 -0800

    Task 2 finished

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+  * main - Prints the name of the program compiled.
+  *
+  * Return: 0 for success and other numbers otherwise.
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
