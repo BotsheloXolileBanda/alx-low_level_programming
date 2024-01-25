cf7bee04e2faf987657acb0592c035abc57e3575
BotsheloXolileBanda <bxbanda@gmail.com>
2024-01-25 06:33:07 -0800

    Task 1 completed at first try

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - Prints integers with set strings.
+  *
+  * @separator: String to print between numbers.
+  * @n: Argument count.
+  * Return: No value.
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int b;
+
+	va_list argus;
+
+	va_start(argus, n);
+
+	if (separator)
+	{
+		for (b = 0; b < n; b++)
+		{
+			if (b < (n - 1))
+			{
+				printf("%d%s", va_arg(argus, int), separator);
+			}
+			else
+			{
+				printf("%d", va_arg(argus, int));
+			}
+		}
+	}
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			printf("%d", va_arg(argus, int));
+		}
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,5 @@
 #include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
