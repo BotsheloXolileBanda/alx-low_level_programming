a5dbb62241d1841b61d99c9868a17902c8e5e3cf
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-04 22:50:04 -0800

    Task 0 done, included printing contents of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * print_listint - Sends the contents of a list to stdout.
+  *
+  * @h: Head node to start list.
+  * Return: The number of nodes.
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t x;
+
+	for (x = 0; h != NULL; x++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (x);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+A project on the workings of a subset of linear data structures: Singly linked lists. It includes tasks to test algorithms to perform operations such as insertion, deletion inter alia
