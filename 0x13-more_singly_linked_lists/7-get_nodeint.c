1d1173e52050dbe041acc526978de98ec6d1b804
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-06 06:16:55 -0800

    Finished task 7, creating a function that retrivesa certain node in the list.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - Returns at node specified.
+  *
+  * @head: First node.
+  * @index: Address of node.
+  * Return: The node found, or NULL.
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int fetch = 0;
+	int fg = 0;
+	listint_t *tm;
+
+	while (head != NULL)
+	{
+		fetch++;
+		if (fetch == index)
+		{
+			fg++;
+			tm = head;
+		}
+		head = head->next;
+	}
+
+	if (fg)
+	{
+		return (tm);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
