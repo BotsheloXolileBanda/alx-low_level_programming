c810e191fb1706ac59f656e1fa02c4d5270f7fa6
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-06 03:30:49 -0800

    Task 6 done- pops head of list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * pop_listint - Deletes the start of a list.
+  *
+  * @h: Head node.
+  * Return: Content of head node.
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *hold;
+	int ncpy;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		hold = (*head)->next;
+		ncpy = (*head)->n;
+		free(*head);
+		*head = hold;
+
+		return (ncpy);
+	}
+}
