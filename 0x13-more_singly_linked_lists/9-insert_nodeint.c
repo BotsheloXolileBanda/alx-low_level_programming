cdf2c7336570a2e7a72514dc8623e60684ea0573
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-06 19:51:05 -0800

    Task 9 done, inserts a node at position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+  * add_nodeint_at_index - Adds a node at given point.
+  *
+  * @head: Start of list.
+  * @idx: Point to insert node.
+  * @n: Data of new node.
+  * Return: New node or NULL.
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int x = 0;
+	int fg = 0;
+	listint_t *tmphd = *head;
+
+	while (tmphd != NULL)
+	{
+		if (x == idx)
+		{
+			tmphd->n = n;
+			fg++;
+			break;
+		}
+		tmphd = tmphd->next;
+		x++;
+	}
+
+	if (fg)
+	{
+		return (tmphd);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
