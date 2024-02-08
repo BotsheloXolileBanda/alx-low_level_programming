39dc1c2808b5e3b31e4bdecb342d850c6b1482e5
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-08 10:27:42 -0800

    Task 2 completed, a function checking for a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * get_bit - Obtains the status of a bit at an index.
+  *
+  * @n: The number to check.
+  * @index: Bit to check.
+  * Return: 0, 1 or -1.
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int msk = 1;
+
+	int newn = n >> index;
+
+	if (msk & newn)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
