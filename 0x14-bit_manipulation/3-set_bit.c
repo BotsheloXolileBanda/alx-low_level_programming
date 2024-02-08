b498aba1ca9a0740c9778644f0d4fa6a4cdc6a6e
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-08 11:03:17 -0800

    Task 3 done, a function that sets a bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_bit - Sets a bit to 1 in a number.
+  *
+  * @n: Number to change.
+  * @index: Bit to change.
+  * Return: The new number.
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int msk = 1 << index;
+
+	*n = (*n | msk);
+
+	return (*n);
+}
