b90169f1cbef9addc6e8077a30e06e147d5e1d23
BotsheloXolileBanda <bxbanda@gmail.com>
2024-02-08 19:59:36 -0800

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+  * flip_bits - Counts the different bits of two numbers.
+  *
+  * @n: First number.
+  * @m: Second number.
+  * Return: The number counted.
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bts = 0;
+
+	int res = (n ^ m);
+
+	if (res = 0)
+	{
+		return (0);
+	}
+	else
+	{
+		while (res)
+		{
+			bts++;
+			res = res & (res - 1);
+		}
+		return (bts);
+	}
+}
