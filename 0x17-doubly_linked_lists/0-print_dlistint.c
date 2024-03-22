5dbf73d9f3b302c5516515cd41d99b7270b04a77
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-22 10:19:00 -0700

    Task 0 done, a task traversing and displaying data in a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * print_dlistint - Prints all node values of list.
+  *
+  * @h: Head node.
+  * Return: Number of nodes.
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (node_count);
+}
